28e010dece462028e595f8c7e4891494cdfd1a77
Hummara Bashir <bashirh@state.gov>
2022-11-10 16:05:47 -0500

    new commits

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DisplayAllPosts from './components/DisplayAllPosts';
+
+function App() {
+  return (
+    <div className="App">
+      <DisplayAllPosts />
+    </div>
+  );
+}
+
+export default App;
